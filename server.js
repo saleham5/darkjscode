cb1bbf62dc63a46f6a7921c279473363db37d0cc
Sal <almhameed5@gmail.com>
2023-12-27 22:28:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,23 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+const cheerio = require('cheerio');
+
+// Inside your route handler
+const modifiedHtml = modifyHtml(responseFromDorarNet);
+res.send(modifiedHtml);
+
+function modifyHtml(html) {
+  const $ = cheerio.load(html);
+  const switchButton = $('.switch_btn');
+
+  if (switchButton.length > 0) {
+    switchButton.click(); // Simulate a click event
+  } else {
+    console.log('Switch button not found');
+  }
+
+  return $.html();
+}
